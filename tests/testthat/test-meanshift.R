8591b570e6778156aadd7bbeb6d92cb59d08c8a2
Jack Krebsbach <krebsbachjackson@gmail.com>
2024-07-15 08:48:16 -0400

    update tests

--- tests/testthat/test-meanshift.R
+++ tests/testthat/test-meanshift.R
@@ -3,12 +3,11 @@ test_that("meanshift function works", {
   test_image <- matrix(runif(100*100*3), nrow=100, ncol=300)
   
   # Run meanshift
-  result <- segment(test_image, spatialRadius = 5, rangeRadius = 5, minDensity = 50)
+  result <- meanshift(test_image, radiusS = 5, radiusR = 5, minDensity = 50)
   
   # Check that the result has the expected structure
   expect_type(result, "list")
-  expect_named(result, c("segmented", "labels", "nb_regions"))
-  expect_equal(dim(result$segmented), dim(test_image))
-  expect_equal(dim(result$labels), c(100, 100))
-  expect_type(result$nb_regions, "integer")
+  expect_named(result, c("segmentedImage", "labelImage", "nbRegions"))
+  expect_equal(dim(result$segmentedImage), dim(test_image))
+  expect_type(result$nbRegions, "integer")
 })
